33d841b40e4fbba4f4eebcc1f80993e212ef90a5
Jasim-10 <mdjasimansari010@gmail.com>
2026-05-26 22:07:07 +0530

    Developer

---
+++ javaScript/script.js
@@ -0,0 +1,91 @@
+// console.log("Hello");
+
+// console.error("this is error");
+
+
+// console.warn("This is warning");
+
+
+// console.info("Server Started Succesfully");     
+
+// let user = {
+//     name:"Jasim",
+//     email:"jasim@gmail.com",
+//     course:"JS"
+// };
+// console.table(user);
+
+
+
+// console.group("User Info");
+// console.log("Name: John");
+// console.log("email: jasim@gmail.com")
+// console.log("Course: JS");
+// console.groupEnd();
+
+
+// console.time("loopTime");
+// for(let i=0; i<100000;i++){}
+// console.timeEnd("loopTime");
+
+// // console.clear();
+
+
+
+// let name = "Ritik Rajput";
+// console.log(name);
+
+// let str = "JavaScript";
+// let str2 = str.slice(0,4);
+// console.log(str2);
+// console.log(str);
+
+
+// let myName = "jasim";
+// console.log(`Hello ${myName} how are you`);
+
+
+// let fruits = "apple, banana, grapes";
+// console.log(fruits.split(","));                 
+
+
+// let str = "I love HTML";
+// console.log(str.replace("HTML","JS"));
+// let msg = "hello how are you, are you fine";
+// console.log(msg.replace("e","w"));
+// console.log(msg.replaceAll("e","w"));
+
+
+// let text = "Learning JavaScript is fun";
+// console.log(text.includes("fun"))
+
+
+// let a = 12;
+// let b = a;
+// a = a+b;
+
+// let c = [1,2,3];
+// let d = c;
+// c.pop();
+
+
+// let a = "";    //null
+// let b;          //undefined
+
+
+// let a = Number.MAX_SAFE_INTEGER;
+
+// let a = 9007199254740991n;
+// console.log(a+4n);
+
+
+
+// let name = prompt("name ?");
+// console.log(name);
+
+
+// 1. Input two integers and print their sum.
+// let num1 = Number(prompt("Enter the first Number: "));
+// let num2 = Number(prompt("Enter the second Number:"));
+// let sum = num1 + num2;
+// console.log(sum);
