97f855f425713c0e5a78b2a93e92a3a612bf1035
Jasim-10 <mdjasimansari010@gmail.com>
2026-05-26 21:15:52 +0530

    Developer

--- javaScript/scipt.js
+++ javaScript/scipt.js
@@ -86,3 +86,13 @@
 
 //  Operatore in js
 
+let a = Number(prompt("Enter the first number: "));
+let b = Number(prompt("Enter the second number: "));
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+
+
