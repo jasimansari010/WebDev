0556d6fbdbcc702ecb5ec6f9540eb827e177aa03
Jasim-10 <mdjasimansari010@gmail.com>
2026-07-19 04:57:03 +0530

    Reels Project

--- Project-1/script.js
+++ Project-1/script.js
@@ -1,111 +1,121 @@
 const reels = [
   {
+    ismuted:true,
     username: "its_aarav",
     likeCount: 12450,
     isLiked: false,
     commentCount: 342,
     shareCount: 185,
     isFollowed: false,
-    caption: "Sunset rides hit different 🌅🏍️",
+    caption: "🍷❌ No Alcohol. No Smoke",
     video: "./Reels/b1.mp4",
     userprofile: "https://i.pravatar.cc/150?img=1",
   },
   {
+    ismuted:true,
     username: "ananya._official",
     likeCount: 28976,
     isLiked: true,
     commentCount: 912,
     shareCount: 426,
     isFollowed: true,
-    caption: "Weekend coffee + coding = perfect combo ☕💻",
+    caption: "💃 Chikni Chameli Vibes... Full Energy, Zero Hesitation",
     video: "./Reels/g1.mp4",
     userprofile: "https://i.pravatar.cc/150?img=5",
   },
   {
+    ismuted:true,
     username: "rohanx_07",
     likeCount: 56891,
     isLiked: false,
     commentCount: 1435,
     shareCount: 987,
     isFollowed: true,
-    caption: "Found this hidden paradise while hiking 🌿⛰️",
+    caption: "💯 Discipline is must",
     video: "./Reels/b2.mp4",
     userprofile: "https://i.pravatar.cc/150?img=8",
   },
   {
+    ismuted:true,
     username: "priya.vibes",
     likeCount: 19870,
     isLiked: true,
     commentCount: 521,
     shareCount: 301,
     isFollowed: false,
-    caption: "Discipline beats motivation every time 💪",
+    caption: "❤️✨ Mera Lal Suit Karda... Nazar Na Lage.",
     video: "./Reels/g2.mp4",
     userprofile: "https://i.pravatar.cc/150?img=10",
   },
   {
+    ismuted:true,
     username: "aditya.codes",
     likeCount: 41230,
     isLiked: false,
     commentCount: 765,
     shareCount: 652,
     isFollowed: true,
-    caption: "Homemade pasta in under 20 minutes 🍝",
+    caption: "👑 Aura +9999",
     video: "./Reels/b3.mp4",
     userprofile: "https://i.pravatar.cc/150?img=12",
   },
   {
+    ismuted:true,
     username: "sneha_clicks",
     likeCount: 17564,
     isLiked: true,
     commentCount: 438,
     shareCount: 512,
     isFollowed: false,
-    caption: "Top VS Code extensions every developer should know 🚀",
+    caption: "🕺🎶 dance give me fun",
     video: "./Reels/g3.mp4",
     userprofile: "https://i.pravatar.cc/150?img=15",
   },
   {
+    ismuted:true,
     username: "karan.fitlife",
     likeCount: 63820,
     isLiked: true,
     commentCount: 1674,
     shareCount: 1204,
     isFollowed: true,
-    caption: "The sound of waterfalls is pure therapy 💙",
+    caption: "Ignore the girls chase the destiny💙",
     video: "./Reels/b4.mp4",
     userprofile: "https://i.pravatar.cc/150?img=18",
   },
   {
+    ismuted:true,
     username: "ishita.diaries",
     likeCount: 34781,
     isLiked: false,
     commentCount: 832,
     shareCount: 721,
     isFollowed: false,
-    caption: "Tried the trending dance challenge 💃🔥",
+    caption: "🥹❤️ Love You, Mera Bachcha... Always & Forever.",
     video: "./Reels/g4.mp4",
     userprofile: "https://i.pravatar.cc/150?img=20",
   },
   {
+    ismuted:true,
     username: "rahul.edits",
     likeCount: 89210,
     isLiked: true,
     commentCount: 2198,
     shareCount: 1845,
     isFollowed: true,
-    caption: "Tag your friend who does this every single day 😂",
+    caption: "😏 Baap Se Hoshiyari",
     video: "./Reels/b5.mp4",
     userprofile: "https://i.pravatar.cc/150?img=24",
   },
   {
+    ismuted:true,
     username: "kavya._xo",
     likeCount: 26795,
     isLiked: false,
     commentCount: 603,
     shareCount: 390,
     isFollowed: false,
-    caption: "Late-night guitar sessions 🎸✨",
+    caption: "🚀💙 Dil kudiyan nhi kude ke nam kar diya",
     video: "./Reels/g5.mp4",
     userprofile: "https://i.pravatar.cc/150?img=28",
   },
@@ -114,21 +124,20 @@ const reels = [
 
 let allReels = document.querySelector('.all-reels');
 
-
-
-
 function addData() {
   let sum = '';
   reels.forEach((elem,idx)=>{
   sum = sum + `
   <div class="reel">
-    <video autoplay loop muted src="${elem.video}"></video>
-
+    <video autoplay loop ${elem.ismuted?'muted':''} src="${elem.video}"></video>
+     <div class="mute" id=${idx}>
+        ${elem.ismuted?'<i class="ri-volume-mute-fill"></i':'<i class="ri-volume-up-line"></i'}>
+      </div>
     <div class="bottom">
         <div class="user">
             <img src="${elem.userprofile}" alt="">
             <h4>${elem.username}</h4>
-            <button>${elem.isFollowed?'Unfollow':'Follow'}</button>
+            <button id=${idx} class='follow'>${elem.isFollowed?'Unfollow':'Follow'}</button>
         </div>
         <h3>${elem.caption}</h3>
     </div>
@@ -162,17 +171,37 @@ addData();
 
 allReels.addEventListener('click',function(dets){
 
-  if(!reels[dets.target.id].isLiked){
-    reels[dets.target.id].likeCount++;
-    reels[dets.target.id].isLiked = true;
-  }else{
-    reels[dets.target.id].likeCount--;
-    reels[dets.target.id].isLiked = false;
+
+  if(dets.target.className == 'like'){
+    if(!reels[dets.target.id].isLiked){
+      reels[dets.target.id].likeCount++;
+      reels[dets.target.id].isLiked = true;
+    }else{
+      reels[dets.target.id].likeCount--;
+      reels[dets.target.id].isLiked = false;
+    }
+    addData();
   }
-  
-  addData();
-  
-  
+
+
+  if(dets.target.className == 'follow'){
+    if(!reels[dets.target.id].isFollowed){
+      reels[dets.target.id].isFollowed = true;
+    }else{
+      reels[dets.target.id].isFollowed = false;
+    }
+    addData();
+  }
+
+
+  if(dets.target.className == 'mute'){
+    if(!reels[dets.target.id].ismuted){
+      reels[dets.target.id].ismuted = true;
+    }else{
+      reels[dets.target.id].ismuted = false;
+    }
+    addData();
+  }
 });
 
 
